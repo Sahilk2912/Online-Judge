75768edca789c95ba063583274c6e7e6598b37d4
Sahil Kurambhatti <sahilkurambhatti@gmail.com>
2024-07-21 14:08:12 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000; // Use environment variable or default to 3000
+const DB_URI =
+  process.env.MONGO_URL || "mongodb://localhost:27017/Online-Judge"; // Use environment variable or default
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`)
-})
+// Middleware to parse JSON
+app.use(express.json());
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+};
+
+startServer();
